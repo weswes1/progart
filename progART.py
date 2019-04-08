b7173b6b095ca1c896a9ddf1f79db992596fa7a6
Wesley Guergah <wguergah10556@g.ucla.edu>
2019-04-08 12:29:06 -0700

    made some further changes

--- progART.py
+++ progART.py
@@ -16,15 +16,29 @@ m = image.shape[0] # Rows
 n = image.shape[1] # Columns
 
 
-# Seth Spike Hacks / Cinderella trails
-
-
 # Create the background color. it will be light brown.
-# # # # # # # # # # # # # # # # # # # # # # # # 
+
+# # # # # # # # # # # # # # # # # # # # # # # #  
 for i in range(0,m):
 	for j in range(0,n):
 		image[i,j]=[0,.100,.203]
 # # # # # # # # # # # # # # # # # # # # # # # # 
+
+
+# JD's loop using 
+x_0 = 78
+y_0 = 41
+d = 75
+
+for i in range(0,10000):
+	xi = random.uniform(0,1)
+	th = random.uniform(0,1)*2*math.pi
+	r = d*math.sqrt(xi/(1-xi))
+	x = int(x_0+r*math.cos(th))
+	y = int(y_0+r*math.sin(th))
+
+	if (x>= 0 and x<500 and  y>=0 and y<500):
+		image[int(x),int(y)]=[1,1,0]
 
 
 
@@ -108,15 +122,14 @@ for row in range(0,75):
 	guh+=1
 
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-
-
-
-
-
+############################################ 
 # Working on the EYES
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+
+
+
+
+##########################################
 
 
 
@@ -226,7 +239,9 @@ for i in range(0,7):
 
 
 
-## Making the Flower portrait in the back right corner of the screen #
+## Making the Flower portrait in the back right corner of the screen ##
+
+
 
 for i in range(20,100):
 	image[30+i,400]=[1,1,1]
@@ -244,23 +259,115 @@ for i in range(55,125):
 	for j in range(405,475):
 		image[i,j]=[0,0.5,0.5]
 
-
 for j in range(0,30):
 	for i in range(0,5):
 		image[123-j,440+i]=[0.3,0,0.8]
 
 
+# Rose petals
+
+count = 1;
+level = 0;
+
+# starting point will be (level,)
+
+
+while count <= 40:
+	print(int(math.ceil(count/2)))
+	for index in range(0,int(math.ceil(count/2))):
+			image[level+60,442+index]=[.8**count,.5**index,03]
+			image[level+60,442-index]=[0,.180,.5**index]
+			image[100-level-2,442+index]=[.210,.12*index,.140]
+			image[100-level-2,442-index]=[.210,.5**index,.140]
+	level+=1;
+	count+=2;
 
 
 
-###
-#print(image.dtype)
+count = 1;
+level = 0;
+
+while count <= 40:
+	for index in range(0,int(math.ceil(count/2))):
+			image[level+80,442+index]=[.5**index,0,0]
+			image[level+90,442-index]=[.5**index,.5**index,.5**index]
+	level+=1;
+	count+=2;
+
+
+count = 1;
+level = 0;
+
+
+while count <= 40:
+	for index in range(0,int(math.ceil(count/2))):
+			image[level+90,430-index]=[.2**index,.1**index,0]
+	level+=1;
+	count+=2;
+
+	
+
+
+# Making a chain for the lanter lantern
+
+for i in range(0,55):
+	for j in range(40,45):
+		image[i,j]=[0,0,0]
+
+# Making the container of the candle
+
+for i in range(50,55):
+	for j in range(10,80):
+		image[i+5,j]=[0,0,0]
+		image[i+80,j]=[0,0,0]
+		image[50+j,i+25]=[0,0,0]
+		image[50+j,64-i]=[0,0,0]
+
+
+for i in range(46,50):
+	for j in range(34,50):
+		image[i+80,j]=[0,0,0]
+
+
+# Candle
+for i in range(80,126):
+	for j in range(40,45):
+		image[i,j]=[1,.3,.4]
+
+
+# Candle Fire
+for i in range(75,85):
+	for j in range(35,50):
+		image[i,j]=[1,0,0]
+
+for i in range(78,82):
+	for j in range(40,45):
+		image[i,j]=[1,1,0]
+
+for x in range(0,10):
+	image[74,38+x]=[1,1,0]
+
+for x in range(0,8):
+	image[73,39+x]=[1,1,0]
+
+for x in range(0,3):
+	image[72,42+x]=[1,1,0]
+
+
+# Lantern glass effect
+for z in range(0,5):
+	for i in range(0,10):
+		for j in range(0,20):
+			image[129-z-i,13+j]=[0,0,0]
+			image[129-z-i,75-j]=[0,0,0]
+			image[58+z+i,13+j]=[0,0,0]
+			image[58+z+i,75-j]=[0,0,0]
 
 
 
-#gaussianBlur = skimage.filters.gaussian(image,sigma=2)
-#plt.imshow(gaussianBlur, interpolation='nearest')
-#plt.show()
-
-plt.imshow(image, interpolation='nearest')
+gaussianBlur = skimage.filters.gaussian(image,sigma=2)
+plt.imshow(gaussianBlur, interpolation='nearest')
 plt.show()
+
+#plt.imshow(image, interpolation='nearest')
+#plt.show()
