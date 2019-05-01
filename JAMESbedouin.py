286e85c0f119b55d574ec570e68db563f025c9a1
Wesley Guergah <wguergah10556@g.ucla.edu>
2019-05-01 15:28:52 -0700

    more detail on the James bedouin portrait

--- JAMESbedouin.py
+++ JAMESbedouin.py
@@ -13,20 +13,15 @@ import skimage
 black = [0,0,0]
 white = [1,1,1]
 blue = [0,0,1]
-
+blonde = [.98,.941,.745]
+sblonde = [.97,.91,.83]
+pink = [.8,.38,.56]
 jamesHouse = np.zeros((500,500,3))
 
 
 
 m = jamesHouse.shape[0] # Rows
 n = jamesHouse.shape[1] # Columns
-
-
-# Creates a background color of white
-for i in range(0,m):
-	for j in range(0,n):
-		jamesHouse[i,j] = white
-
 
 # Makes a circle with center located at center, color of color, and radius of rad
 # if pixel is less than rad pixels from the center
@@ -36,6 +31,7 @@ def makeCircle(rad,center,color):
 		for j in range(center[1]-rad,center[1]+rad):
 			if (math.ceil(math.pow((i-center[0])**2 + (j-center[1])**2,.5)) <= rad):
 				jamesHouse[i,j] = color
+
 # Makes a rectangnle with height, width, and color centered at coordinates of center 	
 
 def makeRect(height,width,color,center):
@@ -44,17 +40,128 @@ def makeRect(height,width,color,center):
 			jamesHouse[i,j]=color
 
 
+# Creates a background color of white
+
+def averagemask(rowstart,colstart,width,factor=1.0/9.0,t=1):
+	for i in range(0,t):
+		for i in range(rowstart,rowstart+width):
+			for j in range(colstart,colstart+width):
+				jamesHouse[i,j] = factor*(jamesHouse[i,j]+jamesHouse[i+1,j]+jamesHouse[i-1,j]+jamesHouse[i,j-1]+jamesHouse[i+1,j-1]+jamesHouse[i-1,j-1]+jamesHouse[i,j+1]+jamesHouse[i+1,j+1]+jamesHouse[i-1,j+1])
+
+
+
+
+
+
+for i in range(0,m):
+	for j in range(0,n):
+		jamesHouse[i,j] = [random.uniform(0,1)/2,0,random.uniform(0,1)/2]
 
 
 makeCircle(60,[150,250],[.8,.71,.54])		# Head
-makeRect(30,20,[.8,.71,.54],[200,250])		# Neck
-makeCircle(30,[259,150],black)			# Shoulder pad 1
-makeCircle(30,[259,350],black)			# Shoulder pad 2
-makeRect(120,90,[0,0,0],[350,250])			# Making the man's garb
-makeRect(75,75,[0,0,0],[100,400])			# Making the frame for the arabic calligraphy
-makeRect(65,65,[1,1,1],[100,400])			# """""""""""""""""""""""""""""""""""""""""""
 
-# VVVVVV Creating the painting -- It will be made using many circles VVVVVVV
+
+
+## Hair Section VVV VVV VVV VVV VVV VVV VVV VVV VVV
+for z in range(0,4):
+	for j in range(0,5):
+		x_0 = 110-5*z
+		y_0 = 210+j*20
+		d = 5
+		for i in range(0,1000):
+			xi = random.uniform(0,1)
+			th = random.uniform(0,1)*2*math.pi
+			r = d*math.sqrt(xi/(1-xi))
+			x = int(x_0+r*math.cos(th))
+			y = int(y_0+r*math.sin(th))
+
+			if (x>= 0 and x<500 and  y >= 0 and y < 500):
+				jamesHouse[int(x),int(y)] = blonde
+
+x_0 = 80
+y_0 = 250
+d = 5
+for i in range(0,1000):
+	xi = random.uniform(0,1)
+	th = random.uniform(0,1)*2*math.pi
+	r = d*math.sqrt(xi/(1-xi))
+	x = int(x_0+r*math.cos(th))
+	y = int(y_0+r*math.sin(th))
+
+	if (x>= 0 and x<500 and  y >= 0 and y < 500):
+		jamesHouse[int(x),int(y)] = blonde
+
+
+
+x_0 = 122
+y_0 = 198
+d = 5
+for i in range(0,1000):
+	xi = random.uniform(0,1)
+	th = random.uniform(0,1)*2*math.pi
+	r = d*math.sqrt(xi/(1-xi))
+	x = int(x_0+r*math.cos(th))
+	y = int(y_0+r*math.sin(th))
+
+	if (x>= 0 and x<500 and  y >= 0 and y < 500):
+		jamesHouse[int(x),int(y)] = blonde
+
+
+x_0 = 122
+y_0 = 298
+d = 5
+for i in range(0,1000):
+	xi = random.uniform(0,1)
+	th = random.uniform(0,1)*2*math.pi
+	r = d*math.sqrt(xi/(1-xi))
+	x = int(x_0+r*math.cos(th))
+	y = int(y_0+r*math.sin(th))
+
+	if (x>= 0 and x<500 and  y >= 0 and y < 500):
+		jamesHouse[int(x),int(y)] = blonde
+
+
+x_0 = 82
+y_0 = 227
+d = 5
+for i in range(0,1000):
+	xi = random.uniform(0,1)
+	th = random.uniform(0,1)*2*math.pi
+	r = d*math.sqrt(xi/(1-xi))
+	x = int(x_0+r*math.cos(th))
+	y = int(y_0+r*math.sin(th))
+
+	if (x>= 0 and x<500 and  y >= 0 and y < 500):
+		jamesHouse[int(x),int(y)] = blonde
+
+x_0 = 82
+y_0 = 273
+d = 5
+for i in range(0,1000):
+	xi = random.uniform(0,1)
+	th = random.uniform(0,1)*2*math.pi
+	r = d*math.sqrt(xi/(1-xi))
+	x = int(x_0+r*math.cos(th))
+	y = int(y_0+r*math.sin(th))
+
+	if (x>= 0 and x<500 and  y >= 0 and y < 500):
+		jamesHouse[int(x),int(y)] = blonde
+
+
+
+makeRect(30,20,[.8,.71,.54],[200,250])		# Neck
+
+makeCircle(30,[259,150],pink)				# Shoulder pad 1
+makeCircle(30,[259,350],pink)				# Shoulder pad 2
+makeRect(135,90,pink,[365,250])			# Making the man's garb
+makeRect(90,20,pink,[350,140])			# Making the man's sleeve
+makeRect(90,20,pink,[350,360])			# Making the man's sleeve
+
+makeRect(75,75,[0,0,0],[100,400])			# Making the frame for the arabic calligraphy
+makeRect(65,65,[.95,.95,.8],[100,400])			# """""""""""""""""""""""""""""""""""""""""""
+
+# VVVVVV Creating the painting --- It will be made using many circles VVVVVVV ---
+
 makeCircle(3,[105,355],black)
 makeCircle(4,[109,352],black)
 makeCircle(5,[112,353],black)
@@ -76,7 +183,6 @@ makeCircle(3,[108,377],black)
 makeCircle(3,[110,378],black)
 makeCircle(3,[112,378],black)
 
-
 makeCircle(4,[114,377],black)
 makeCircle(4,[116,377],black)
 makeCircle(4,[118,377],black)
@@ -95,7 +201,6 @@ makeCircle(4,[138,377],black)
 makeCircle(4,[140,377],black)
 makeCircle(5,[142,379],black)
 makeCircle(6,[144,381],black)
-
 
 makeCircle(6,[144,383],black)
 makeCircle(5,[143,386],black)
@@ -214,6 +319,85 @@ makeCircle(1,[81,377],black)
 # ^^^^^^^^ ^^^^^^^^ ^^^^^^^^ ^^^^^^^^ ^^^^^^^^ ^^^^^^^^ Making the portrait
 
 
+# VVVVVVVVVVVVVV Sunset Painting
+
+makeRect(55,55,black,[151,99])
+makeRect(50,51,[.06,.08,.38],[150,99])
+
+makeCircle(10,[120,70],[.96,.94,.76])
+makeCircle(10,[120,75],[.06,.08,.38])
+
+for  i in range(100,165):
+	for j in range(50,150):
+		if (random.uniform(0,1)<.05):
+			jamesHouse[i,j]=[1,1,1]
+
+
+
+x_0 = 160
+y_0 = 100
+d = 5
+
+for i in range(0,15000):
+	xi = random.uniform(0,1)
+	th = random.uniform(0,1)*2*math.pi
+	r = d*math.sqrt(xi/(1-xi))
+	x = int(x_0+r*math.cos(th))
+	y = int(y_0+r*math.sin(th))
+
+	if (x >= 0 and x <500 and  y >= 0 and y < 500):
+		jamesHouse[int(x),int(y)] = [1,0,0]
+
+x_0 = 160
+y_0 = 100
+d = 5
+
+for i in range(0,5000):
+	xi = random.uniform(0,1)
+	th = random.uniform(0,1)*2*math.pi
+	r = d*math.sqrt(xi/(1-xi))
+	x = int(x_0+r*math.cos(th))
+	y = int(y_0+r*math.sin(th))
+
+	if (x >= 0 and x <500 and  y >= 0 and y < 500):
+		jamesHouse[int(x),int(y)] = [1,.64,0]
+
+
+
+x_0 = 160
+y_0 = 100
+d = 5
+
+for i in range(0,1000):
+	xi = random.uniform(0,1)
+	th = random.uniform(0,1)*2*math.pi
+	r = d*math.sqrt(xi/(1-xi))
+	x = int(x_0+r*math.cos(th))
+	y = int(y_0+r*math.sin(th))
+
+	if (x >= 0 and x < 500 and  y >= 0 and y < 500):
+		jamesHouse[int(x),int(y)] = [1,1,0]
+
+
+for i in range(165,200):
+	for j in range(48,150):
+		jamesHouse[i,j]=[0,0,.3]
+
+
+for i in range(166,200):
+	for j in range(81,121):
+		if (random.uniform(0,1) < .03 ):
+			jamesHouse[i,j]=[1,1,1]
+
+averagemask(100,50,100,t=3)
+
+
+
+
+# ^^^^^^^^^^ Sunset
+# ^^^^^^^^^^ Sunset
+# ^^^^^^^^^^ Sunset
+
 ## VVV Making the eye section VVV
 ## VVV Making the eye section VVV
 ## VVV Making the eye section VVV
@@ -227,7 +411,6 @@ makeCircle(4,[136,225],black)
 makeCircle(3,[136,227],black)
 makeCircle(2,[136,229],black)
 
-
 makeCircle(2,[136,277],black)
 makeCircle(3,[136,279],black)
 makeCircle(4,[136,281],black)
@@ -235,7 +418,6 @@ makeCircle(5,[136,283],black)
 makeCircle(4,[136,285],black)
 makeCircle(3,[136,287],black)
 makeCircle(2,[136,289],black)
-
 
 makeCircle(4,[136,283],white)
 makeCircle(4,[136,223],white)
@@ -247,7 +429,6 @@ makeCircle(2,[136,223],black)
 # ^^^^ Making the eye section ^^^^
 ## ^^^^ Making the eye section ^^^^
 ## ^^^^ Making the eye section ^^^^
-
 
 # VVV Making the nose VVVV
 # VVV Making the nose VVVV
@@ -269,6 +450,119 @@ makeCircle(2,[179,242],black)
 ## ^^^^ Making the nose section ^^^^
 ## ^^^^ Making the nose section ^^^^
 
+### VVVVV James' T Shirt Patterns
+
+for k in range(0,30):
+	for i in range(0,5):
+		d = float(i)
+		makeCircle(4*i,[375-20*i+k,250+k],[0,d/10.0,d/5])
+		makeCircle(4*i,[375+20*i+k,250+k],[d/5.0,0,d/10.0])
+		makeCircle(4*i,[375+k,250+10*i+k],[0,d/10.0,0])
+		makeCircle(4*i,[375+k,250-10*i+k],[d/10.0,0,d/10.0])
+
+
+### ^^^^ James' T Shirt patterns
+
+# random.uniform(0,1)/2,0,random.uniform(0,1)/2]
+### VVVVVV James' Beard Goatee VVVVVV
+
+for i in range(0,10):
+	makeCircle(i,[290-10*i,250],sblonde)
+
+### ^^^ James' Beard Goatee ^^^^ ######
+
+
+# Lining for the shirt
+
+for i in range(230,442):
+		jamesHouse[i,339]=black
+		jamesHouse[i,160]=black
+
+
+# [.8,.71,.54] color for finger
+
+### VVV fingers VVVVVV fingers VVVVVV fingers VVVVVV fingers VVV
+makeCircle(3,[442,345],[.8,.71,.54])
+makeCircle(3,[446,352],[.8,.71,.54])
+makeCircle(3,[442,352],[.8,.71,.54])
+makeCircle(3,[446,352],[.8,.71,.54])
+makeCircle(3,[442,359],[.8,.71,.54])
+makeCircle(3,[446,359],[.8,.71,.54])
+makeCircle(3,[442,366],[.8,.71,.54])
+makeCircle(3,[446,366],[.8,.71,.54])
+makeCircle(3,[442,372],[.8,.71,.54])
+makeCircle(3,[446,372],[.8,.71,.54])
+
+makeCircle(3,[442,153],[.8,.71,.54])
+makeCircle(3,[446,125],[.8,.71,.54])
+makeCircle(3,[442,125],[.8,.71,.54])
+makeCircle(3,[446,132],[.8,.71,.54])
+makeCircle(3,[442,132],[.8,.71,.54])
+makeCircle(3,[446,132],[.8,.71,.54])
+makeCircle(3,[442,139],[.8,.71,.54])
+makeCircle(3,[446,139],[.8,.71,.54])
+makeCircle(3,[442,139],[.8,.71,.54])
+makeCircle(3,[446,146],[.8,.71,.54])
+makeCircle(3,[442,146],[.8,.71,.54])
+makeCircle(3,[446,146],[.8,.71,.54])
+
+### ^^^^ Fingers Fingers Fingers ^^^^^^^^^^^^^^^^^^^^^^^^
+### ^^^^ Fingers Fingers Fingers ^^^^^^^^^^^^^^^^^^^^^^^^
+### ^^^^ Fingers Fingers Fingers ^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+### VV Third Portrait, trippy patterns VV
+### VV Third Portrait, trippy patterns VV
+
+
+makeCircle(47,[250,449],blue)
+makeCircle(45,[250,449],white)
+for i in range(0,10):
+	if i%2==0:
+		makeCircle(45-4*i,[250,449],white)
+	else:
+		makeCircle(45-4*i,[250,449],blue)
+
+
+
+
+for k in range(0,10):
+	for i in range(0,4):
+			d = float(i)
+			makeCircle(i,[250-8*i+k,440+k],[0,d/10.0,d/5])
+			makeCircle(i,[250+8*i+k,440+k],[d/5.0,0,d/10.0])
+			makeCircle(i,[250+k,440+8*i+k],[0,d/10.0,0])
+			makeCircle(i,[250+k,440-8*i+k],[d/10.0,0,d/10.0])
+
+for k in range(0,10):
+	for i in range(0,4):
+			d = float(i)
+			makeCircle(i,[250-8*i-k,440+k],[0,d/10.0,d/5])
+			makeCircle(i,[250+8*i-k,440+k],[d/5.0,0,d/10.0])
+			makeCircle(i,[250-k,440+8*i+k],[0,d/10.0,0])
+			makeCircle(i,[250-k,440-8*i+k],[d/10.0,0,d/10.0])
+
+for k in range(0,10):
+	for i in range(0,4):
+			d = float(i)
+			makeCircle(i,[250-8*i+k,450+k],[0,d/10.0,d/5])
+			makeCircle(i,[250+8*i+k,450+k],[d/5.0,0,d/10.0])
+			makeCircle(i,[250+k,450+8*i+k],[0,d/10.0,0])
+			makeCircle(i,[250+k,450-8*i+k],[d/10.0,0,d/10.0])
+
+for k in range(0,10):
+	for i in range(0,4):
+			d = float(i)
+			makeCircle(i,[250-8*i-k,450+k],[0,d/10.0,0])
+			makeCircle(i,[250+8*i-k,450+k],[d/5.0,0,0])
+			makeCircle(i,[250-k,450+8*i+k],[0,d/10.0,0])
+			makeCircle(i,[250-k,450-8*i+k],[d/10.0,0,0])
+
+
+
+
+### ^^ Third Portrait, trippy patterns ^^
+### ^^ Third Portrait, trippy patterns ^^
 
 plt.imshow(jamesHouse, interpolation='nearest')
 plt.show()
